c04b764ebb3f154035cda1b13cec189ffc4350c2
guanjiaxu <m15540097929@163.com>
2025-11-06 21:15:29 +0800

    test

---
+++ hello.c
@@ -0,0 +1,21 @@
+/*********************************************************************************
+ *      Copyright:  (C) 2025 Chen Chongyuan<1309010124@qq.com>
+ *                  All rights reserved.
+ *
+ *       Filename:  hello.c
+ *    Description:  This file 
+ *                 
+ *        Version:  1.0.0(06/11/25)
+ *         Author:  Chen Chongyuan <1309010124@qq.com>
+ *      ChangeLog:  1, Release initial version on "06/11/25 21:12:08"
+ *                 
+ ********************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+	printf("hello world\n");
+
+	return 0;
+}
